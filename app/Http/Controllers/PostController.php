5447cd287d7a620b4d9ecde953810952ad8f666a
WC Jeff <wc2014920@gmail.com>
2021-07-03 19:44:15 +0800

    PostController.php changed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Arr;
-use http\Env\Response;
+//use http\Env\Response;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -17,13 +17,13 @@ class PostController extends Controller
 
         $drug_name = Arr::get($data, 'drug_table.0.drug_name');//drug_table.0.drug_name
         $drug_pic_arr = Arr::get($data, 'drug_table.0.fig');//drug_table.0.drug_name
-        $drug_pic_str = Arr::pull($drug_pic_arr, '0');
-//        dd($pic);//drug_table.0.fig
-        $drug_pic_str;
+        $drug_pic_str = Arr::get($drug_pic_arr, '0');
+//        dd($drug_pic_str);//drug_table.0.fig
+//        $drug_pic_str;
 //        print_r($request);
 
         return view('home', [
-            'pic_url' => $drug_pic_str,
+            'pic_url' => $drug_pic_str,//ok
             'drug_name'=>$drug_name
         ]);
     }
